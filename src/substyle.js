23a0757cfd407a58d07cf44e115e49bca0482e7c
Jan-Felix <schwarz@mpango.de>
2016-07-27 10:27:42 +0200

    implemented chaining ability for substyle calls

--- src/substyle.js
+++ src/substyle.js
@@ -10,53 +10,61 @@ import map from 'lodash/fp/map'
 import compose from 'lodash/fp/compose'
 
 
-export default function substyle({ style, className }, selectedKeys) {
+function createSubstyle(closureProps) {
+  function substyle(props, selectedKeys) {
+    const style = (closureProps.style || props.style) && merge({}, closureProps.style, props.style)
+    const className = props.className || closureProps.className
 
-  if(!selectedKeys) {
-    selectedKeys = []
-  } else if(typeof selectedKeys === 'string') {
-    selectedKeys = [selectedKeys]
-  } else if(Object.prototype.toString.call(selectedKeys) === '[object Object]') {
-    selectedKeys = keys(selectedKeys).reduce(
-      (keys, key) => keys.concat(selectedKeys[key] ? [key] : []),
-      []
-    )
-  }
-
-  invariant(
-    Array.isArray(selectedKeys), 
-    'Second parameter must be a string, an array of strings, a plain object with boolean values, or a falsy value'
-  )
-
-  const baseClassName = className && className.split(' ')[0]
-  const toElementClassNames = map(key => baseClassName + '__' + key)
-  const toModifierClassNames = map(key => baseClassName + '--' + key.substring(1))
-
-  const modifierKeys = filter(isModifier, selectedKeys)
-  const elementKeys = filter(isElement, selectedKeys)
-
-  const hoistElementStyles = (style) => values(pickNestedStyles(style, elementKeys))
-  const hoistModifierStyles = (style) => values(pickNestedStylesRecursive(style, modifierKeys))
-  const hoistElementStylesFromEach = elementKeys.length > 0 ? compose(flatten, map(hoistElementStyles)) : identity
-
-  return {
-
-    ...( style && { 
-      style : merge({},
-        ...hoistElementStylesFromEach([ style, ...hoistModifierStyles(style) ])
+    if(!selectedKeys) {
+      selectedKeys = []
+    } else if(typeof selectedKeys === 'string') {
+      selectedKeys = [selectedKeys]
+    } else if(Object.prototype.toString.call(selectedKeys) === '[object Object]') {
+      selectedKeys = keys(selectedKeys).reduce(
+        (keys, key) => keys.concat(selectedKeys[key] ? [key] : []),
+        []
       )
-    }),
+    }
 
-    ...( className && { 
-      className : (elementKeys.length === 0 ?
-        [ className, ...toModifierClassNames(modifierKeys) ] :
-        toElementClassNames(elementKeys)
-      ).join(' ')
+    invariant(
+      Array.isArray(selectedKeys), 
+      'Second parameter must be a string, an array of strings, a plain object with boolean values, or a falsy value'
+    )
+
+    const baseClassName = className && className.split(' ')[0]
+    const toElementClassNames = map(key => baseClassName + '__' + key)
+    const toModifierClassNames = map(key => baseClassName + '--' + key.substring(1))
+
+    const modifierKeys = filter(isModifier, selectedKeys)
+    const elementKeys = filter(isElement, selectedKeys)
+
+    const hoistElementStyles = (style) => values(pickNestedStyles(style, elementKeys))
+    const hoistModifierStyles = (style) => values(pickNestedStylesRecursive(style, modifierKeys))
+    const hoistElementStylesFromEach = elementKeys.length > 0 ? compose(flatten, map(hoistElementStyles)) : identity
+
+    return createSubstyle({
+
+      ...( style && { 
+        style : merge({},
+          ...hoistElementStylesFromEach([ style, ...hoistModifierStyles(style) ])
+        )
+      }),
+
+      ...( className && { 
+        className : (elementKeys.length === 0 ?
+          [ className, ...toModifierClassNames(modifierKeys) ] :
+          toElementClassNames(elementKeys)
+        ).join(' ')
+      })
+
     })
-
   }
 
+  Object.assign(substyle, closureProps)
+  return substyle
 }
+
+export default createSubstyle({})
 
 const isModifier = key => key[0] === '&'
 const isElement = negate(isModifier)
