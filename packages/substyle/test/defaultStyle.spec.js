384d52c8016f64d4d237755f7166529d79be0799
Jan-Felix <schwarz@mpango.de>
2017-08-23 14:39:28 +0200

    added test for providing props decorator via context

--- packages/substyle/test/defaultStyle.spec.js
+++ packages/substyle/test/defaultStyle.spec.js
@@ -1,191 +1,191 @@
-import { expect } from "chai";
-import { shallow, mount } from "enzyme";
-import { createElement, Component } from "react";
+import { expect } from 'chai'
+import { shallow, mount } from 'enzyme'
+import { createElement, Component } from 'react'
 
-import "./utils/dom";
-import defaultStyle from "../src/defaultStyle";
-import createSubstyle from "../src/createSubstyle";
+import './utils/dom'
+import defaultStyle from '../src/defaultStyle'
+import createSubstyle from '../src/createSubstyle'
 import {
   ENHANCER_CONTEXT_NAME,
-  PROPS_DECORATOR_CONTEXT_NAME
-} from "../src/types";
+  PROPS_DECORATOR_CONTEXT_NAME,
+} from '../src/types'
 
-describe("`defaultStyle` higher-order component factory", () => {
+describe('`defaultStyle` higher-order component factory', () => {
   const MyComponent = ({ style, ...rest }) =>
     createElement(
-      "div",
+      'div',
       { ...style, ...rest },
-      createElement("span", { ...style("nested") })
-    );
+      createElement('span', { ...style('nested') })
+    )
 
-  it("should inject a substyle instance for the `style` prop", () => {
-    const MyEnhancedComponent = defaultStyle({ color: "red" })(MyComponent);
-    const wrapper = shallow(createElement(MyEnhancedComponent));
-    const styleProp = wrapper.props().style;
-    expect(styleProp).to.be.a.instanceOf(Function);
-    expect(styleProp.style).to.deep.equal({ color: "red" });
-  });
+  it('should inject a substyle instance for the `style` prop', () => {
+    const MyEnhancedComponent = defaultStyle({ color: 'red' })(MyComponent)
+    const wrapper = shallow(createElement(MyEnhancedComponent))
+    const styleProp = wrapper.props().style
+    expect(styleProp).to.be.a.instanceOf(Function)
+    expect(styleProp.style).to.deep.equal({ color: 'red' })
+  })
 
-  it("should merge styles provided by the component user with default styles", () => {
-    const MyEnhancedComponent = defaultStyle({ color: "red" })(MyComponent);
+  it('should merge styles provided by the component user with default styles', () => {
+    const MyEnhancedComponent = defaultStyle({ color: 'red' })(MyComponent)
     const wrapper = shallow(
       createElement(MyEnhancedComponent, {
-        style: { cursor: "pointer" }
+        style: { cursor: 'pointer' },
       })
-    );
-    const styleProp = wrapper.props().style;
+    )
+    const styleProp = wrapper.props().style
     expect(styleProp.style).to.deep.equal({
-      color: "red",
-      cursor: "pointer"
-    });
-  });
+      color: 'red',
+      cursor: 'pointer',
+    })
+  })
 
-  it("should also work if a substyle instance is provided as `style`", () => {
-    const MyEnhancedComponent = defaultStyle({ color: "red" })(MyComponent);
+  it('should also work if a substyle instance is provided as `style`', () => {
+    const MyEnhancedComponent = defaultStyle({ color: 'red' })(MyComponent)
     const wrapper = shallow(
       createElement(MyEnhancedComponent, {
         style: createSubstyle({
-          style: { cursor: "pointer" }
-        })
+          style: { cursor: 'pointer' },
+        }),
       })
-    );
-    const styleProp = wrapper.props().style;
+    )
+    const styleProp = wrapper.props().style
     expect(styleProp.style).to.deep.equal({
-      color: "red",
-      cursor: "pointer"
-    });
-  });
+      color: 'red',
+      cursor: 'pointer',
+    })
+  })
 
-  it("should accept a function mapping props to default styles as first argument", () => {
+  it('should accept a function mapping props to default styles as first argument', () => {
     const MyEnhancedComponent = defaultStyle(props => ({ color: props.color }))(
       MyComponent
-    );
+    )
     const wrapper = shallow(
       createElement(MyEnhancedComponent, {
-        style: { cursor: "pointer" },
-        color: "black"
+        style: { cursor: 'pointer' },
+        color: 'black',
       })
-    );
-    const styleProp = wrapper.props().style;
+    )
+    const styleProp = wrapper.props().style
     expect(styleProp.style).to.deep.equal({
-      color: "black",
-      cursor: "pointer"
-    });
-  });
+      color: 'black',
+      cursor: 'pointer',
+    })
+  })
 
-  it("should take a modifier selection function as second argument", () => {
+  it('should take a modifier selection function as second argument', () => {
     const MyEnhancedComponent = defaultStyle(
       {
-        color: "red",
+        color: 'red',
 
-        "&readOnly": {
-          opacity: 0.5
-        }
+        '&readOnly': {
+          opacity: 0.5,
+        },
       },
       props => ({
-        "&readOnly": props.readOnly
+        '&readOnly': props.readOnly,
       })
-    )(MyComponent);
+    )(MyComponent)
     const wrapper = shallow(
       createElement(MyEnhancedComponent, { readOnly: true })
-    );
-    const styleProp = wrapper.props().style;
+    )
+    const styleProp = wrapper.props().style
     expect(styleProp.style).to.deep.equal({
-      color: "red",
-      opacity: 0.5
-    });
-  });
+      color: 'red',
+      opacity: 0.5,
+    })
+  })
 
-  it("should apply the selected modifiers also on the style supplied by the user", () => {
-    const MyEnhancedComponent = defaultStyle({ color: "red" }, props => ({
-      "&readOnly": props.readOnly
-    }))(MyComponent);
+  it('should apply the selected modifiers also on the style supplied by the user', () => {
+    const MyEnhancedComponent = defaultStyle({ color: 'red' }, props => ({
+      '&readOnly': props.readOnly,
+    }))(MyComponent)
     const wrapper = shallow(
       createElement(MyEnhancedComponent, {
         readOnly: true,
         style: {
-          "&readOnly": {
-            opacity: 0.5
-          }
-        }
+          '&readOnly': {
+            opacity: 0.5,
+          },
+        },
       })
-    );
-    const styleProp = wrapper.props().style;
+    )
+    const styleProp = wrapper.props().style
     expect(styleProp.style).to.deep.equal({
-      color: "red",
-      opacity: 0.5
-    });
-  });
+      color: 'red',
+      opacity: 0.5,
+    })
+  })
 
-  it("should give precendence to styles supplied by the user, regardless the modifiers specificity", () => {
+  it('should give precendence to styles supplied by the user, regardless the modifiers specificity', () => {
     const MyEnhancedComponent = defaultStyle(
       {
-        "&readOnly": {
-          opacity: 0.5
-        }
+        '&readOnly': {
+          opacity: 0.5,
+        },
       },
       props => ({
-        "&readOnly": props.readOnly
+        '&readOnly': props.readOnly,
       })
-    )(MyComponent);
+    )(MyComponent)
     const wrapper = shallow(
       createElement(MyEnhancedComponent, {
         readOnly: true,
         style: {
-          opacity: 0.7
-        }
+          opacity: 0.7,
+        },
       })
-    );
-    const styleProp = wrapper.props().style;
+    )
+    const styleProp = wrapper.props().style
     expect(styleProp.style).to.deep.equal({
-      opacity: 0.7
-    });
-  });
+      opacity: 0.7,
+    })
+  })
 
-  it("should support depency injection via context for additional HoC to wrap the component", () => {
-    const MyStyledComponent = defaultStyle()(MyComponent);
+  it('should support depency injection via context for additional HoC to wrap the component', () => {
+    const MyStyledComponent = defaultStyle()(MyComponent)
     const wrapInSection = WrappedComponent => props =>
-      createElement("section", {}, createElement(WrappedComponent, props));
+      createElement('section', {}, createElement(WrappedComponent, props))
     const wrapper = shallow(createElement(MyStyledComponent), {
       context: {
-        [ENHANCER_CONTEXT_NAME]: wrapInSection
-      }
-    });
-    expect(wrapper.get(0).type().type).to.equal("section");
-  });
+        [ENHANCER_CONTEXT_NAME]: wrapInSection,
+      },
+    })
+    expect(wrapper.get(0).type().type).to.equal('section')
+  })
 
-  it("should expose the wrapped component instance via `getWrappedInstance`", () => {
+  it('should expose the wrapped component instance via `getWrappedInstance`', () => {
     class MyClassComponent extends Component {
       render() {
-        return createElement("div");
+        return createElement('div')
       }
     }
-    const MyEnhancedClassComponent = defaultStyle()(MyClassComponent);
-    const instance = mount(createElement(MyEnhancedClassComponent)).instance();
-    expect(instance.getWrappedInstance).to.be.a("function");
-    expect(instance.getWrappedInstance()).to.be.an.instanceOf(MyClassComponent);
+    const MyEnhancedClassComponent = defaultStyle()(MyClassComponent)
+    const instance = mount(createElement(MyEnhancedClassComponent)).instance()
+    expect(instance.getWrappedInstance).to.be.a('function')
+    expect(instance.getWrappedInstance()).to.be.an.instanceOf(MyClassComponent)
 
-    const MyFunctionComponent = () => createElement("div");
-    const MyEnhancedFunctionComponent = defaultStyle()(MyFunctionComponent);
+    const MyFunctionComponent = () => createElement('div')
+    const MyEnhancedFunctionComponent = defaultStyle()(MyFunctionComponent)
     const instance2 = mount(
       createElement(MyEnhancedFunctionComponent)
-    ).instance();
-    expect(instance2.getWrappedInstance()).to.not.exist;
-  });
+    ).instance()
+    expect(instance2.getWrappedInstance()).to.not.exist
+  })
 
-  it("should support providing a props decorator function via context", () => {
-    const MyStyledComponent = defaultStyle()(MyComponent);
+  it('should support providing a props decorator function via context', () => {
+    const MyStyledComponent = defaultStyle()(MyComponent)
     const decorateProps = props => ({
-      mapped: "foobar"
-    });
+      mapped: 'foobar',
+    })
     const wrapper = mount(createElement(MyStyledComponent), {
       context: {
-        [PROPS_DECORATOR_CONTEXT_NAME]: decorateProps
-      }
-    });
-    const containerProps = wrapper.find("MyComponent").find("div").props();
-    expect(containerProps).to.not.have.property("style");
-    expect(containerProps).to.not.have.property("className");
-    expect(containerProps).to.have.property("mapped", "foobar");
-  });
-});
+        [PROPS_DECORATOR_CONTEXT_NAME]: decorateProps,
+      },
+    })
+    const containerProps = wrapper.find('MyComponent').find('div').props()
+    expect(containerProps).to.not.have.property('style')
+    expect(containerProps).to.not.have.property('className')
+    expect(containerProps).to.have.property('mapped', 'foobar')
+  })
+})
