a511b0b265fa6363d4a4d60dbb8e3de848b9013f
Jan-Felix <schwarz@mpango.de>
2017-08-23 14:19:03 +0200

    added test for props decorator and allow to provide decorator function via context

--- packages/substyle/src/defaultStyle.js
+++ packages/substyle/src/defaultStyle.js
@@ -1,76 +1,86 @@
 // @flow
-import { createElement, Component } from 'react'
-import hoistStatics from 'hoist-non-react-statics'
-import { identity, isFunction } from 'lodash'
+import { createElement, Component } from "react";
+import hoistStatics from "hoist-non-react-statics";
+import { identity, isFunction } from "lodash";
 
-import createSubstyle from './createSubstyle'
-import { PropTypes, ContextTypes, ENHANCER_CONTEXT_NAME } from './types'
-import type { PropsT, KeysT } from './types'
+import createSubstyle from "./createSubstyle";
+import {
+  PropTypes,
+  ContextTypes,
+  ENHANCER_CONTEXT_NAME,
+  PROPS_DECORATOR_CONTEXT_NAME
+} from "./types";
+import type { PropsT, KeysT } from "./types";
 
 const createDefaultStyle = (
-  defaultStyle?: Object | (props: Object) => Object,
-  getModifiers?: (props: Object) => KeysT,
+  defaultStyle?: Object | ((props: Object) => Object),
+  getModifiers?: (props: Object) => KeysT
 ) => (WrappedComponent: ReactClass) => {
   class WithDefaultStyle extends Component<void, PropsT, void> {
     static WrappedComponent: ReactClass;
 
     constructor(props, context) {
-      super(props, context)
-      this.setWrappedInstance = this.setWrappedInstance.bind(this)
+      super(props, context);
+      this.setWrappedInstance = this.setWrappedInstance.bind(this);
     }
 
     render() {
-      const { style, className, classNames, ...rest } = this.props
+      const { style, className, classNames, ...rest } = this.props;
 
-      const substyle = createSubstyle({ style, className, classNames })
-      const modifiers = getModifiers && getModifiers(rest)
-      const finalDefaultStyle = isFunction(defaultStyle) ? defaultStyle(rest) : defaultStyle
+      const substyle = createSubstyle(
+        { style, className, classNames },
+        this.context[PROPS_DECORATOR_CONTEXT_NAME]
+      );
+      const modifiers = getModifiers && getModifiers(rest);
+      const finalDefaultStyle = isFunction(defaultStyle)
+        ? defaultStyle(rest)
+        : defaultStyle;
 
-      return createElement(
-        this.getWrappedComponent(),
-        {
-          style: substyle(modifiers, finalDefaultStyle),
-          ref: this.setWrappedInstance,
-          ...rest,
-        }
-      )
+      return createElement(this.getWrappedComponent(), {
+        style: substyle(modifiers, finalDefaultStyle),
+        ref: this.setWrappedInstance,
+        ...rest
+      });
     }
 
     getWrappedComponent() {
-      const { [ENHANCER_CONTEXT_NAME]: enhance = identity }: ContextT = this.context
+      const {
+        [ENHANCER_CONTEXT_NAME]: enhance = identity
+      }: ContextT = this.context;
 
       if (this.memoizedEnhance !== enhance) {
-        this.memoizedEnhance = enhance
-        this.enhancedWrappedComponent = enhance(WrappedComponent)
+        this.memoizedEnhance = enhance;
+        this.enhancedWrappedComponent = enhance(WrappedComponent);
       }
 
-      return this.enhancedWrappedComponent || WrappedComponent
+      return this.enhancedWrappedComponent || WrappedComponent;
     }
 
     getWrappedInstance() {
-      return this.wrappedInstance
+      return this.wrappedInstance;
     }
 
     setWrappedInstance(ref) {
-      this.wrappedInstance = ref
+      this.wrappedInstance = ref;
     }
   }
 
-  const wrappedComponentName = WrappedComponent.displayName || WrappedComponent.name
-  WithDefaultStyle.displayName = `withDefaultStyle(${wrappedComponentName})`
+  const wrappedComponentName =
+    WrappedComponent.displayName || WrappedComponent.name;
+  WithDefaultStyle.displayName = `withDefaultStyle(${wrappedComponentName})`;
 
   // define prop types based on WrappedComponent's prop types
   WithDefaultStyle.propTypes = {
     ...WrappedComponent.propTypes,
-    ...PropTypes,
-  }
+    ...PropTypes
+  };
 
-  WithDefaultStyle.contextTypes = ContextTypes
+  WithDefaultStyle.contextTypes = ContextTypes;
 
   // expose WrappedComponent, e.g., for testing purposes
-  WithDefaultStyle.WrappedComponent = WrappedComponent
+  WithDefaultStyle.WrappedComponent = WrappedComponent;
 
-  return hoistStatics(WithDefaultStyle, WrappedComponent)
-}
+  return hoistStatics(WithDefaultStyle, WrappedComponent);
+};
 
-export default createDefaultStyle
+export default createDefaultStyle;
--- packages/substyle/src/types.js
+++ packages/substyle/src/types.js
@@ -1,7 +1,8 @@
 // @flow
-import PT from 'prop-types'
+import PT from "prop-types";
 
-export const ENHANCER_CONTEXT_NAME = '__substyle__Enhancer'
+export const ENHANCER_CONTEXT_NAME = "__substyle__enhancer";
+export const PROPS_DECORATOR_CONTEXT_NAME = "__substyle__propsDecorator";
 
 export type KeysT = string | Array<string> | { [string]: boolean };
 
@@ -9,30 +10,32 @@ export type SubstyleT = (select: KeysT, defaultStyle?: Object) => SubstyleT;
 
 export type StyleT = SubstyleT | Object;
 
-const StylePT = PT.oneOfType([PT.func, PT.object])
+const StylePT = PT.oneOfType([PT.func, PT.object]);
 
 export type ClassNamesT = {
   [string]: string
 };
 
-const ClassNamesPT = PT.objectOf(PT.string)
+const ClassNamesPT = PT.objectOf(PT.string);
 
 export type PropsT = {
   style?: StyleT,
   className?: string,
-  classNames?: ClassNamesT,
+  classNames?: ClassNamesT
 };
 
 export const PropTypes = {
   style: StylePT,
   className: PT.string,
-  classNames: ClassNamesPT,
-}
+  classNames: ClassNamesPT
+};
 
 export type ContextT = {
   [ENHANCER_CONTEXT_NAME]: ?(WrappedComponent: ReactClass) => ReactClass,
+  [PROPS_DECORATOR_CONTEXT_NAME]: ?(props: PropsT) => Object
 };
 
 export const ContextTypes = {
   [ENHANCER_CONTEXT_NAME]: PT.func,
-}
+  [PROPS_DECORATOR_CONTEXT_NAME]: PT.func
+};
