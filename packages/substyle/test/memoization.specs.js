eabb570687868df59425338207d4714e998d1eb0
Jan-Felix <janfelixschwarz@gmail.com>
2018-02-27 09:47:06 +0100

    more memoization tests

--- packages/substyle/test/memoization.specs.js
+++ packages/substyle/test/memoization.specs.js
@@ -1,10 +1,27 @@
 import { expect } from 'chai'
 
-import createSubstyle from '../src/createSubstyle'
+import { createSubstyle } from '../src'
 
 describe('memoization', () => {
   it('should return the very same result for two calls with same modifiers', () => {
     const substyle = createSubstyle({})
-    expect(substyle('foo')).to.equal(substyle('foo')) // equal as in ===, no deep equal!
+
+    // equal as in ===, no deep equal!
+    expect(substyle('foo')).to.equal(substyle('foo'))
+    expect(substyle('foo')).to.equal(substyle(['foo']))
+    expect(substyle('foo')).to.equal(substyle({ foo: true }))
+  })
+
+  it('should return the very same result for two calls with same modifiers and identic default styles', () => {
+    const substyle = createSubstyle({})
+    const defaultStyle = {}
+    expect(substyle('foo', defaultStyle)).to.equal(
+      substyle('foo', defaultStyle)
+    )
+  })
+
+  it('should keep memoizing through chained substyle calls', () => {
+    const substyle = createSubstyle({})
+    expect(substyle('foo')('bar')).to.equal(substyle('foo')('bar'))
   })
 })
