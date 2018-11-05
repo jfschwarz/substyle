26e37c35f797b376bcd9f2eb5c61e58ec4db265c
Jan-Felix <janfelixschwarz@gmail.com>
2018-11-05 23:33:43 +0100

    move back to lodash keys

--- packages/substyle/src/coerceSelection.js
+++ packages/substyle/src/coerceSelection.js
@@ -1,4 +1,5 @@
 // @flow
+import { keys } from 'lodash'
 import type { KeysT } from './types'
 
 const coerceSelection = (select?: KeysT): Array<string> => {
@@ -8,7 +9,7 @@ const coerceSelection = (select?: KeysT): Array<string> => {
     return [select]
   } else if (!Array.isArray(select)) {
     const objSelect: { [string]: boolean } = select // workaround for https://github.com/facebook/flow/issues/5781
-    return Object.keys(select).reduce(
+    return keys(select).reduce(
       (acc, key: string) => acc.concat(objSelect[key] ? [key] : []),
       []
     )
