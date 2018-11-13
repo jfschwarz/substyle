085b1be6ae6c470e8ea5b67749ef5c9288068695
goenning <me@goenning.net>
2018-11-13 19:07:17 +0000

    fix linter

--- packages/substyle/test/utils.spec.js
+++ packages/substyle/test/utils.spec.js
@@ -4,9 +4,9 @@ describe('isPlainObject', () => {
   [
     { input: {}, expected: true },
     { input: {a: 1}, expected: true },
-    { input: new Object(), expected: true },
-    { input: new Object({ a: 1}), expected: true },
-    { input: new Object({}), expected: true },
+    { input: new Object(), expected: true }, // eslint-disable-line no-new-object
+    { input: new Object({ a: 1}), expected: true }, // eslint-disable-line no-new-object
+    { input: new Object({}), expected: true }, // eslint-disable-line no-new-object
     { input: 2, expected: false },
     { input: 'Name', expected: false },
     { input: new Date(), expected: false }
