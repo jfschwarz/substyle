67b7a13f4a59107f48bde0f8b47a806ca3a2ee03
Jan-Felix <janfelixschwarz@gmail.com>
2020-04-27 18:03:23 +0200

    cleanup exports

--- packages/substyle/src/index.js
+++ packages/substyle/src/index.js
@@ -6,13 +6,7 @@ import inline from './inline'
 import type { SubstyleT as Substyle } from './types'
 import useStyles from './useStyles'
 
-export {
-  useStyles,
-  createSubstyle,
-  PropsDecoratorProvider,
-  defaultPropsDecorator,
-  inline,
-}
+export { createSubstyle, PropsDecoratorProvider, defaultPropsDecorator, inline }
 
 export default useStyles
 
