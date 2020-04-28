c4636bffb3c4f8eea8373eeb3031bb78a18db777
Jan-Felix <janfelixschwarz@gmail.com>
2020-04-28 10:40:36 +0200

    fix memoization and simplify arg handling

--- packages/substyle/src/useStyles.js
+++ packages/substyle/src/useStyles.js
@@ -4,6 +4,7 @@ import { useContext, useMemo } from 'react'
 
 import { PropsDecoratorContext } from './PropsDecoratorProvider'
 import createSubstyle from './createSubstyle'
+import coerceSelection from './coerceSelection'
 import {
   type ClassNamesT,
   type ModifiersT,
@@ -25,11 +26,11 @@ type InternalOverrideT = {|
 
 type Args = [StyleT, OverrideT] | [StyleT, OverrideT, ModifiersT]
 
-const useStyles = (...args: Args) => {
-  const [defaultStyle, { style, className, classNames }, modifiers] = parseArgs(
-    ...args
-  )
-
+const useStyles = (
+  defaultStyle?: StyleT,
+  { style, className, classNames }: OverrideT,
+  modifiers?: ModifiersT
+) => {
   const propsDecorator = useContext(PropsDecoratorContext)
 
   const substyle = useMemo(
@@ -37,98 +38,16 @@ const useStyles = (...args: Args) => {
     [style, className, classNames, propsDecorator]
   )
 
-  return useMemo(() => substyle(modifiers, defaultStyle), [
+  // Modifiers are recalculated on every render. To leverage memoization we need to map the
+  // selected modifiers (array of strings) to something that keeps its identity if the set of
+  // modifiers does not change.
+  // We join them to a string, assuming users won't change the order of keys in the modifiers object.
+  const selectedModifiers = coerceSelection(modifiers)
+  return useMemo(() => substyle(selectedModifiers, defaultStyle), [
     substyle,
-    modifiers,
+    selectedModifiers.join(','),
     defaultStyle,
   ])
-}
-
-const defaultModifiers = Object.freeze({})
-const defaultOverrides = Object.freeze({})
-
-const parseArgs = (...args: Args): [StyleT, InternalOverrideT, ModifiersT] => {
-  if (args.length === 1) {
-    const [defaultStyle] = args
-
-    return [defaultStyle, defaultOverrides, defaultModifiers]
-  }
-
-  if (args.length === 2) {
-    const [defaultStyle, overrides] = args
-
-    return [defaultStyle, ensureOverrides(overrides), defaultModifiers]
-  }
-
-  invariant(
-    args.length === 3,
-    `useStyles must be called with either 1, 2, or 3 arguments, not ${args.length}.`
-  )
-
-  const [defaultStyle, overrides, modifiers] = args
-
-  return [defaultStyle, ensureOverrides(overrides), modifiers]
-}
-
-const ensureOverrides = (overrides): InternalOverrideT => {
-  invariant(
-    typeof overrides !== 'string',
-    'Overrides must be an object, not "string".'
-  )
-
-  invariant(!Array.isArray(overrides), 'Overrides cannot be an array.')
-
-  const style = ensureStyle(overrides)
-
-  const className = style?.className
-    ? ensureClassName(style)
-    : ensureClassName(overrides)
-  const classNames = ensureClassNames(overrides)
-
-  return { classNames, className, style }
-}
-
-const ensureClassName = (overrides): ?string => {
-  if (!overrides) {
-    return
-  }
-
-  if (!overrides.className) {
-    return
-  }
-
-  invariant(
-    typeof overrides.className === 'string',
-    `"className" property must be "string" not "${typeof overrides.className}".`
-  )
-
-  return overrides.className
-}
-
-const ensureClassNames = (overrides): ?ClassNamesT => {
-  if (!overrides.classNames) {
-    return
-  }
-
-  invariant(
-    typeof overrides.classNames !== 'boolean',
-    '"classNames" property cannot be a boolean.'
-  )
-
-  return overrides.classNames
-}
-
-const ensureStyle = (overrides): StyleT | SubstyleT | void => {
-  if (!overrides.style) {
-    return
-  }
-
-  invariant(
-    typeof overrides.style !== 'boolean',
-    '"style" property cannot be a boolean'
-  )
-
-  return overrides.style
 }
 
 export default useStyles
