a67caa657e6cf58549464a578f8b6a3c03cf3894
Jan-Felix <janfelixschwarz@gmail.com>
2020-04-28 11:29:00 +0200

    save double memo

--- packages/substyle/src/useStyles.js
+++ packages/substyle/src/useStyles.js
@@ -3,7 +3,6 @@ import { useContext, useMemo } from 'react'
 
 import { PropsDecoratorContext } from './PropsDecoratorProvider'
 import createSubstyle from './createSubstyle'
-import coerceSelection from './coerceSelection'
 import { type ModifiersT, type StyleT, type PropsT } from './types'
 
 const useStyles = (
@@ -18,18 +17,7 @@ const useStyles = (
     [style, className, classNames, propsDecorator]
   )
 
-  // Modifiers are recalculated on every render. To leverage memoization we need to map the
-  // selected modifiers (array of strings) to something that keeps its identity if the set of
-  // modifiers does not change.
-  // We join them to a string, assuming users won't change the order of keys in the modifiers object.
-  const selectedModifiers = coerceSelection(modifiers)
-  const joinedModifiers = selectedModifiers.join(',')
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  return useMemo(() => substyle(selectedModifiers, defaultStyle), [
-    substyle,
-    joinedModifiers,
-    defaultStyle,
-  ])
+  return substyle(selectedModifiers, defaultStyle)
 }
 
 export default useStyles
