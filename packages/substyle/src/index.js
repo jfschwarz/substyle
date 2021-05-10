a6ae005fd54a0099689111f27dbe13e5505ac72c
Jan-Felix <janfelixschwarz@gmail.com>
2021-05-10 17:31:53 +0200

    run prettier

--- packages/substyle/src/index.js
+++ packages/substyle/src/index.js
@@ -1,12 +1,20 @@
 // @flow
-import PropsDecoratorProvider, { PropsDecoratorContext } from './PropsDecoratorProvider'
+import PropsDecoratorProvider, {
+  PropsDecoratorContext,
+} from './PropsDecoratorProvider'
 import createSubstyle from './createSubstyle'
 import defaultPropsDecorator from './defaultPropsDecorator'
 import inline from './inline'
 import type { SubstyleT as Substyle, PropsT as StylingProps } from './types'
 import useStyles from './useStyles'
 
-export { createSubstyle, PropsDecoratorProvider, PropsDecoratorContext, defaultPropsDecorator, inline }
+export {
+  createSubstyle,
+  PropsDecoratorProvider,
+  PropsDecoratorContext,
+  defaultPropsDecorator,
+  inline,
+}
 
 export default useStyles
 
--- packages/substyle/src/memoize.js
+++ packages/substyle/src/memoize.js
@@ -3,23 +3,22 @@ import type { KeysT, SubstyleT } from './types'
 
 const EMPTY = {}
 
-const memoize = (substyle: SubstyleT): SubstyleT => (
-  select: KeysT,
-  defaultStyle?: Object
-) => {
-  const cacheKey = defaultStyle || EMPTY
-  substyle.memoize = substyle.memoize || new WeakMap()
-  let mapEntry
-  if (!substyle.memoize.has(cacheKey)) {
-    mapEntry = {}
-    substyle.memoize.set(cacheKey, mapEntry)
-  } else {
-    mapEntry = substyle.memoize.get(cacheKey)
+const memoize =
+  (substyle: SubstyleT): SubstyleT =>
+  (select: KeysT, defaultStyle?: Object) => {
+    const cacheKey = defaultStyle || EMPTY
+    substyle.memoize = substyle.memoize || new WeakMap()
+    let mapEntry
+    if (!substyle.memoize.has(cacheKey)) {
+      mapEntry = {}
+      substyle.memoize.set(cacheKey, mapEntry)
+    } else {
+      mapEntry = substyle.memoize.get(cacheKey)
+    }
+    const selectHash = coerceSelection(select).join(' ')
+    return selectHash in mapEntry
+      ? mapEntry[selectHash]
+      : (mapEntry[selectHash] = substyle(select || [], defaultStyle))
   }
-  const selectHash = coerceSelection(select).join(' ')
-  return selectHash in mapEntry
-    ? mapEntry[selectHash]
-    : (mapEntry[selectHash] = substyle(select || [], defaultStyle))
-}
 
 export default memoize
